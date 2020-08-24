eb87cbad369823dcbe2b83e42a9814381c583eb7
vsafontev <vladislavsafontev@gmail.com>
2020-08-24 10:37:35 +0300

    two commit

--- app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
+++ app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
@@ -2,8 +2,10 @@ package com.example.mymessenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -14,5 +16,16 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view) {
+        EditText messageView = (EditText) findViewById(R.id.message);
+        String messageText = messageView.getText().toString();
+        /*Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);*/
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_TEXT, messageText);
+        String chooserTitle = getString(R.string.chooser);
+        Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+//        startActivity(intent);
+        startActivity(chosenIntent);
     }
 }
